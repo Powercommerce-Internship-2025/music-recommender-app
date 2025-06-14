d0edb697df0c3ecc842ebaeeaba0485fbcccb42a
realmadrid2 <tohran1@etf.unsa.ba>
2025-06-14 13:33:03 +0200

    feat: navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+/*  
+  Navbar za čitavu app
+*/
+
+function Navbar() {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between">
+        <Link to="/" className="text-white font-bold">Music Recommender</Link>
+        <div>
+          <Link to="/login" className="text-white mr-4">Login</Link>
+          <Link to="/register" className="text-white">Register</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
