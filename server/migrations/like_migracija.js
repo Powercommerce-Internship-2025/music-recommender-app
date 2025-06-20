193a1b589385acece5542ecaa487158264f05356
realmadrid2 <tohran1@etf.unsa.ba>
2025-06-20 16:57:44 +0200

    feat: migration of tables for albums, artists and likes

---
+++ server/migrations/like_migracija.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/*
+  Migracija za kreiranje tabele likes
+*/
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('likes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      rating: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      albumId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'albums',
+          key: 'id',
+        },
+      },
+      artistId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'artists',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    await queryInterface.addIndex('likes', ['userId', 'albumId'], { unique: true });
+    await queryInterface.addIndex('likes', ['userId', 'artistId'], { unique: true });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('likes');
+  },
+};
